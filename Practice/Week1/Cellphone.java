836de7c08075cf6566b6505a5fcce99be137a99f
owen_co_pal <owenezekiel.anchola@lorma.edu>
2026-01-21 14:03:30 +0800

    oop-1

---
+++ Practice/Week1/Cellphone.java
@@ -0,0 +1,15 @@
+package Practice.Week1;
+public class Cellphone{
+    String Platform;
+    String OS;
+
+
+
+    void print(){
+    System.out.printf("""
+            Platform:%s 
+            OS:%s
+            """ , Platform , OS);
+}
+}
+
--- Practice/Week1/CellphoneMain.java
+++ Practice/Week1/CellphoneMain.java
@@ -1,11 +1,17 @@
-package Practice;
+package Practice.Week1;
 public class CellphoneMain{
     public static void main(String[] args) {
+    System.out.println("Cellphones");
+    System.out.println();
     Cellphone cellphone1 = new Cellphone();
     cellphone1.Platform = "Android";
     cellphone1.OS = "Color OS";
     Cellphone cellphone2 = new Cellphone();
     cellphone2.Platform = "Iphone";
     cellphone2.OS = "IOS";
+
+    cellphone1.print();
+    System.out.println();
+    cellphone2.print();
     }
 }
